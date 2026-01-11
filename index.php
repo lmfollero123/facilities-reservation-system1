a890dde61f61ad297e6514ceec77785ad33ebbbf
lmfollero123 <follero.luismiguel.noora@gmail.com>
2026-01-11 23:05:28 +0800

    Create Index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Root entry point for the Facilities Reservation System
+ * Redirects to the public home page
+ */
+
+// Redirect to the public home page
+header('Location: resources/views/pages/public/home.php');
+exit;
