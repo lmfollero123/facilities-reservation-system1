d3a010769d6c4a642b2636967b277122d5a6caea
lmfollero123 <follero.luismiguel.noora@gmail.com>
2026-01-12 19:14:12 +0800

    Fix

--- resources/views/layouts/guest_layout.php
+++ resources/views/layouts/guest_layout.php
@@ -41,7 +41,7 @@ if ($isHomePage || $isPublicPage) {
         $customCss = '/public/css/style.css';
     }
     // Cache-busting: Update this version number when CSS changes are deployed
-    $cssVersion = '2.0';
+    $cssVersion = '2.1';
     ?>
     <link rel="stylesheet" href="<?= htmlspecialchars($customCss); ?>?v=<?= $cssVersion; ?>">
     <style>
