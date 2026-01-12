57b4da803947199f9fa08c64b87e2ff1ca065b5a
lmfollero123 <follero.luismiguel.noora@gmail.com>
2026-01-12 20:15:55 +0800

    f

--- config/database.php
+++ config/database.php
@@ -10,6 +10,12 @@
  const DB_NAME = 'cprf_facilities_reservation';
  const DB_USER = 'cprf_root';        // default XAMPP user
  const DB_PASS = '#Ej9+LqgMpteCp17';
+
+
+//  const DB_HOST = 'localhost';
+//  const DB_NAME = 'facilities_reservation';
+//  const DB_USER = 'oot';        // default XAMPP user
+//  const DB_PASS = '';
 /**
  * Returns a shared PDO instance.
  */
