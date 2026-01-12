cb2646220a58f3667c972676ba3794674a570c57
lmfollero123 <follero.luismiguel.noora@gmail.com>
2026-01-12 20:51:19 +0800

    Update guest_layout.php

--- resources/views/layouts/guest_layout.php
+++ resources/views/layouts/guest_layout.php
@@ -41,7 +41,7 @@ if ($isHomePage || $isPublicPage) {
         $customCss = '/public/css/style.css';
     }
     // Cache-busting: Update this version number when CSS changes are deployed
-    $cssVersion = '5.0';
+    $cssVersion = '6.0';
     ?>
     <link rel="stylesheet" href="<?= htmlspecialchars($customCss); ?>?v=<?= $cssVersion; ?>">
     <style>
