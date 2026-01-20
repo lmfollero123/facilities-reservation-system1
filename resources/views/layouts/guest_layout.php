9e328c9ce0c845d7b316211ae773f0302b9f343a
lmfollero123 <follero.luismiguel.noora@gmail.com>
2026-01-20 21:40:39 +0800

    Css fix

--- resources/views/layouts/guest_layout.php
+++ resources/views/layouts/guest_layout.php
@@ -52,7 +52,7 @@ if ($isHomePage || $isPublicPage) {
         $customCss = '/public/css/style.css';
     }
     // Cache-busting: Update this version number when CSS changes are deployed
-    $cssVersion = '5.2';
+    $cssVersion = '8.0';
     ?>
     <link rel="stylesheet" href="<?= htmlspecialchars($customCss); ?>?v=<?= $cssVersion; ?>">
     <style>
